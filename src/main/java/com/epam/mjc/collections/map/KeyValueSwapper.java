1c566b82a0661b0da582e73cd0ab3c4b322ce123
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-30 17:08:02 +0400

    j

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> swapped = new HashMap<>();
+
+        for (Map.Entry<Integer,String> entry : sourceMap.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+
+            if (!swapped.containsKey(value)) {
+                swapped.put(value,key);
+            }
+        }
+        return swapped;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        String[] w = sentence.split("\\W+");
+
+        Map<String, Integer> reps = new HashMap<>();
+        if (!sentence.isEmpty()) {
+
+        for (String word : w) {
+            String lowercaseWord = word.toLowerCase();
+
+            reps.put(lowercaseWord, reps.getOrDefault(lowercaseWord, 0) + 1);
+        }
+
+            return reps;
+        }
+        else {
+            return reps;
+        }
     }
 }
