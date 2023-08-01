b50c7c517d493b3004763ab6f8e09ce1662c46be
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-01 22:19:15 +0400

    fasdfadas

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -9,14 +9,6 @@ public class WordRepetitionMapCreator {
 
         Map<String, Integer> wordRepetitionMap = new HashMap<>();
 
-        StringTokenizer tokenizer = new StringTokenizer(sentence, " ");
-
-        while (tokenizer.hasMoreTokens()) {
-            String word = tokenizer.nextToken().toLowerCase();
-
-            wordRepetitionMap.put(word, wordRepetitionMap.getOrDefault(word, 0) + 1);
-        }
-
         return wordRepetitionMap;
 
     }
